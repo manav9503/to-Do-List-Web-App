a64dbf6fd91895fb6a1c9eb6dadbd2a90128ad19
Manav Wadikar <manav9503wadikar@gmail.com>
2025-05-27 20:21:26 +0530

    Delete todo_project directory

--- todo_project/urls.py
+++
@@ -1,8 +0,0 @@
-# todo_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo_app.urls')),
-]
