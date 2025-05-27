2dbf3688c70059ef8efbcc44c942a81659b27965
Manav Wadikar <manav9503wadikar@gmail.com>
2025-05-27 20:21:16 +0530

    Delete todo_app directory

--- todo_app/urls.py
+++
@@ -1,9 +0,0 @@
-# todo_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
-]
